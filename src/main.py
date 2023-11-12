5042970fd8282c09abaabad6d078c101b5c14489
y10ab1 <yuehpo.peng@gmail.com>
2023-11-13 01:10:39 +0800

    update test code and rename eff model module(test->eff_model)

--- src/main.py
+++ src/main.py
@@ -21,8 +21,10 @@ from torcheval.metrics.classification import BinaryAUROC
 from torcheval.metrics.functional import binary_auprc
 from torcheval.metrics.functional import binary_auroc
 from sklearn.metrics import precision_score, recall_score
-from model import MY_VGG16
 
+from model import MY_VGG16
+from vgg_cbam import VGG16_CBAM
+from eff_model import EFF_CBAM
 
 torch.manual_seed(8868)
 
@@ -35,13 +37,16 @@ def main(args):
 
         
         
-    vgg_model = models.vgg16(pretrained=True)
-    model = MY_VGG16(vgg_model)
+    # vgg_model = models.vgg16(pretrained=True)
+    eff_model = models.efficientnet_b7(pretrained=True)
+    # model = MY_VGG16(vgg_model)
+    # model = VGG16_CBAM(vgg_model)
+    model = EFF_CBAM(eff_model)
 
     print(model)
-    print(model.features_9[1])
     model.to(args.device)
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=5, verbose=True)
 
 
     train_data = Retina_Dataset('train')
@@ -74,7 +79,7 @@ def main(args):
             
                 
             
-        pritn()
+        print()
         # validation
         model.eval()
         with torch.no_grad():
@@ -98,6 +103,9 @@ def main(args):
             df.to_csv('test.csv')
             val_loss = val_loss / len(val_loader)
             print('val_loss: {}'.format(val_loss))
+            
+            scheduler.step(val_loss)
+            
             if val_loss < best_val_loss:
                 best_val_loss = val_loss
                 torch.save(model,'./checkpoint/best_vgg.pt')
@@ -113,9 +121,6 @@ def main(args):
                     TN += 1
             SP = (TN) / (TN+FP)
             
-            # print(target)
-            # print(pred)
-            # print(logits)
             metric = BinaryAccuracy()
             print('Accuracy:', metric(pred, target).item())
             print('Specificity', SP)
@@ -125,6 +130,49 @@ def main(args):
             print('AUROC', binary_auroc(logits, target).item())
             
             print('-----------------------------------------------')
+            
+def test(args):
+    model = torch.load('./checkpoint/best_vgg.pt')
+    model.to(args.device)
+    test_data = Retina_Dataset('test')
+    test_loader = DataLoader(test_data, batch_size=args.batch_size, shuffle=False)
+    with torch.no_grad():
+        target = torch.tensor([])
+        pred = torch.tensor([])
+        logits = torch.tensor([])
+        for i, data in enumerate(test_loader, 0):
+            inputs, labels = data
+            labels = labels.to(args.device)
+            inputs = inputs.to(args.device) 
+            outputs = model(inputs)
+            outputs = outputs.squeeze(-1)
+            outputs_label = outputs > 0.5
+            logits = torch.cat((logits, outputs.cpu()))
+            target = torch.cat((target, labels.cpu()))
+            pred = torch.cat((pred, outputs_label.cpu()))
+        dict = {'labels' : target.tolist(), 'logits' : logits.tolist()}
+        df = pd.DataFrame(dict)
+        df.to_csv('test.csv')
+        target_list = target.tolist()
+        pred_list = pred.tolist()
+        FP = 0
+        TN = 0
+        for z in range(len(pred_list)):
+            if target_list[z] == 0 and pred_list[z] == 1:
+                FP += 1
+            if target_list[z] == 0 and pred_list[z] == 0:
+                TN += 1
+        SP = (TN) / (TN+FP)
+        
+        metric = BinaryAccuracy()
+        print('Accuracy:', metric(pred, target).item())
+        print('Specificity', SP)
+        print('Recall', recall_score(target.tolist(), pred.tolist()))
+        print('Precision', precision_score(target.tolist(), pred.tolist()))
+        print('AUPRC', binary_auprc(logits, target).item())
+        print('AUROC', binary_auroc(logits, target).item())
+        
+        print('-----------------------------------------------')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -133,4 +181,5 @@ if __name__ == "__main__":
     parser.add_argument('--lr', type=float, default=0.0001)
     parser.add_argument('--device', type=str, default='cuda' if torch.cuda.is_available() else 'cpu')
     args = parser.parse_args()
-    main(args)
+    # main(args)
+    test(args)
