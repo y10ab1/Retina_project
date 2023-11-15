098f25afb3745cbfc5530d5231039d66e938eb9a
archielu <43569364+archielu@users.noreply.github.com>
2023-11-15 15:32:06 +0800

    Add files via upload

---
+++ image_preprocessing.py
@@ -0,0 +1,28 @@
+import cv2
+
+
+
+
+import cv2
+green_image = cv2.imread('../dataset/Training_Set/Training_Set/Training/1.png')
+
+clahe = cv2.createCLAHE(clipLimit=1.0, tileGridSize=(8,8))
+b,g,r = cv2.split(green_image)
+b = clahe.apply(b)
+g = clahe.apply(g)
+r = clahe.apply(r)
+image = cv2.merge([b,g,r])
+cv2.imwrite('original_CLAHE.png',image)
+
+green_image[:,:,0] = 0
+green_image[:,:,2] = 0
+b,g,r = cv2.split(green_image)
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+b = clahe.apply(b)
+g = clahe.apply(g)
+r = clahe.apply(r)
+image = cv2.merge([b,g,r])
+
+cv2.imwrite('CLAHE.png',image)
+cv2.imwrite('green.png',green_image)
+
